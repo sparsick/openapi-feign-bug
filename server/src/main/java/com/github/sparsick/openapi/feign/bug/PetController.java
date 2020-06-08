3c4cddf2782e26f66e66235ea6fbb2e57f001d08
sparsick <sparsick@web.de>
2020-06-08 09:07:43 +0200

    improve generator config

--- server/src/main/java/com/github/sparsick/openapi/feign/bug/PetController.java
+++ server/src/main/java/com/github/sparsick/openapi/feign/bug/PetController.java
@@ -1,6 +1,9 @@
 package com.github.sparsick.openapi.feign.bug;
 
 import com.github.sparsick.openapi.feign.bug.api.PetApi;
+import com.github.sparsick.openapi.feign.bug.model.Fish;
+import com.github.sparsick.openapi.feign.bug.model.OneOfFishMammal;
+import com.github.sparsick.openapi.feign.bug.model.Pet;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PetController implements PetApi {
 
+    @Override
+    public ResponseEntity<Pet> petGet() {
+        Pet pet = new Pet();
+        pet.setId(1L);
+        pet.setName("a pet");
+        OneOfFishMammal kindOf = new OneOfFishMammal();
+        Fish fish = new Fish();
+        fish.setLocation("water");
+        kindOf.setFish(fish);
+        pet.setKindOf(kindOf);
+        return new ResponseEntity<>(pet,HttpStatus.OK);
+    }
 }
---
+++ server/src/test/java/com/github/sparsick/openapi/feign/bug/PetControllerTest.java
@@ -0,0 +1,17 @@
+package com.github.sparsick.openapi.feign.bug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class PetControllerTest {
+
+
+    @Test
+    void test(){
+
+    }
+
+}
